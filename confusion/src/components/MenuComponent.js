2b83bfb8761ada68a6ca0bdf503f4ff6d2708afc
ThomasLodato <telodato.16@est.ucab.edu.ve>
2020-09-11 19:45:09 -0400

    React Router

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
+import { Card, CardImg, CardImgOverlay,
+    CardTitle } from 'reactstrap';
 
-    function RenderMenuItem({ dish, onClick }) {
+    function RenderMenuItem ({dish, onClick}) {
         return (
-            <Card key={dish.id} onClick={() => onClick(dish.id)}>
-                  <CardImg width="100%" src={dish.image} alt={dish.name} />
-                  <CardImgOverlay>
-                      <CardTitle>{dish.name}</CardTitle>
-                  </CardImgOverlay>
-                </Card>
+            <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
         );
     }
 
@@ -16,9 +17,9 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
 
         const menu = props.dishes.map((dish) => {
             return (
-              <div  key={dish.id} className="col-12 col-md-5 m-1">
-                  <RenderMenuItem dish={dish} onClick={props.onClick} />
-              </div>
+                <div className="col-12 col-md-5 m-1"  key={dish.id}>
+                    <RenderMenuItem dish={dish} onClick={props.onClick} />
+                </div>
             );
         });
 
