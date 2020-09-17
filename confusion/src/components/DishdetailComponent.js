6b94903dc9a23a24e05e37ef983ce10084533503
ThomasLodato <telodato.16@est.ucab.edu.ve>
2020-09-17 13:11:21 -0400

    React Animation Components

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import { LocalForm, Control, Errors } from 'react-redux-form'
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -97,13 +98,19 @@ class CommentForm extends Component {
     function RenderDish({dish}){
         if (dish != null)
             return(
-                <Card>
-                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
+                <FadeTransform 
+                    in
+                    transformProps={{
+                        exitTransform: 'scale(0.5) translateY(-50%)'
+                    }}>
+                    <Card>
+                        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </FadeTransform>
         );
         else {
             return(
@@ -113,18 +120,25 @@ class CommentForm extends Component {
     }
 
     function RenderComments({comments, postComment, dishId}) {
+        
+        
         const cmts = comments.map((cmm) => {
             return (
-                <li className="list-unstyled" key={cmm.id}>
-                    <p>{cmm.comment}</p>
-                    <p>--{cmm.author}, {new Intl.DateTimeFormat('en-US', { year:'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(cmm.date)))}</p>
-                </li>
+                <Fade in>
+                    <li className="list-unstyled" key={cmm.id}>
+                        <p>{cmm.comment}</p>
+                        <p>--{cmm.author}, {new Intl.DateTimeFormat('en-US', { year:'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(cmm.date)))}</p>
+                    </li>
+                </Fade>
             );
         });
+
         return(
             <Card>
                 <h3>Comments</h3>
-                {cmts}
+                <Stagger in>
+                    {cmts}
+                </Stagger>
                 <CommentForm dishId={dishId} postComment={postComment} />
             </Card>
         );
